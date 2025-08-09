77f4c9bf5977ea3c03b9c301302ea30981ee744c
johandry8919 <rojas.rojas440@gmail.com>
2025-08-08 21:13:36 -0400

    comitt

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/fundayacucho/',
   build: {
-    assetsDir: 'assets', // Asegura que los recursos estén en /assets
+    assetsDir: 'assets',
     outDir: 'dist',
   }
 });
