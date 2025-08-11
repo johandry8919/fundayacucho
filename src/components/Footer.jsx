4cb20cb9e6e5bf2ed008b0ceefc304062e365b1b
johandry8919 <rojas.rojas440@gmail.com>
2025-08-11 10:43:04 -0400

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function Footer() {
   return (
-   <footer className=" text-white">
+   <footer className=" text-black">
     <div className="text-center p-4">
                 © 2025 Copyright:
-                <a className="text-reset fw-bold" href="index.php">
+                <a className=" fw-bold text-black" href="index.php">
                   <img width={40} src="https://fundayacucho.gob.ve/img/logo50ani.ico" alt=""  className="rounded-pill"/> 
                       fundayacucho.gob.ve</a>
-                      <p>Direción General de Tecnología de la Información  </p>
+                      <p className='text-black'>Direción General de Tecnología de la Información  </p>
       </div>
 </footer>
 
