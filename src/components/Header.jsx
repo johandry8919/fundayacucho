6ce9c8ee83fbe536f068156bb9ce71f99a6f3833
johandry8919 <rojas.rojas440@gmail.com>
2025-08-11 00:13:17 -0400

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 export default function Header() {
   return (
     <header className="header">
-        <img width={'100%'} height={155}   className="img" src="./img/cintillo4.png" alt="" />
+        <img width={'100%'} height={155}   className="img" src="./img/cintillo5.png" alt="" />
     </header>
   );
 }
