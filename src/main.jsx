bc520e6e3d45bb89f8c21a9be0ea66a52a86e1fc
johandry8919 <rojas.rojas440@gmail.com>
2025-08-19 10:10:16 -0400

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import router from './routes';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Admin from './pages/Admin';
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/main.css';
 import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <AuthProvider>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
-  </React.StrictMode>
+ 
 );
