f2eb0781abc7852fe5aab967984b2307a5e52efd
johandry8919 <rojas.rojas440@gmail.com>
2025-09-16 22:49:21 -0400

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import { Password } from '@mui/icons-material';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/api'; 
+const API_BASE_URL = 'http://backend-becarios.fundayacucho.gob.ve/api'; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
